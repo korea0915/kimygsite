8601d0189c514abb1e9432e1414d0d1139fbb393
nadabtbs <nadabtbs@gmail.com>
2021-02-09 08:18:10 +0000

    change index.php

--- index.php
+++ index.php
@@ -13,11 +13,15 @@
                         <a href="mailto:nadabtbs@gmail.com">nadabtbs@email.com</a>
                     </div>
                     <p class="lead mb-5">
-                      나에대한 소개
+                      나에대한 소개<br>
+                      저는 21살에 호주로 유학을 가서 28살까지 호주에서 생활을 하엿고 한국에 들어와서 바로 군대에 입대 하였으며 얼마전에 전역을 하였습니다.
+                      &ensp;호주에 있으면서 다양한 사람들을 만나고 생활하면서 여러가지 환경에서 적응하는 법과 넓은 시각을 가질 수 있는 기회였습니다.<br> 
+                      저의 장점으로는 어디서든 잘 적응할 수 있는 적응력과 성실함 입니다. 무슨 일을 하던 책임감을 가지고 할 수 있습니다. 
+                      단점으로는 조금 내성적인 성격와 즉흥적인 성격입니다.                      
                     </p>
                     <div class="social-icons">
-                        <a class="social-icon" href="#"><i class="fab fa-github"></i></a>
-                        <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="social-icon" href="https://github.com/korea0915/kimygsite"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://facebook.com/yeonggwang1"><i class="fab fa-facebook-f"></i></a>
                     </div>
                 </div>
             </section>
@@ -31,7 +35,7 @@
                             <h3 class="mb-0">초보 웹 개발자</h3>
                             <div class="subheading mb-3">PHP와 MYSQL 개발</div>
                             <p>주로 사용하는 언어는 PHP와 MYSQL로 웹개발 하는걸 즐깁니다. &ensp;아직도 부족한게 많다고 느끼고 배움을 게을리 하지 않고 있습니다.</p>
-                            <p class="mb-0">최근 Laravel framework를 익히고 있으며 빠르게 변해가는 트렌드를 따라가기위해 노력하고 배우고 있습니다.</p>
+                            <p class="mb-0">이 사이트는 메인페이지를 제외하고 PHP와 MYSQL로만 만들어 봤습니다.</p>
                         </div>
                         <div class="flex-shrink-0">
                             <span class="text-primary">March 2012 - Present</span>
@@ -42,7 +46,7 @@
                         <div class="flex-grow-1">
                             <h3 class="mb-0">웹 서버</h3>
                             <div class="subheading mb-3">웹서버 이전 작업</div>
-                            <p>(나중에 클럽에스프레소 사이트 사진과 링크)
+                            <p>
                               클럽에스프레소 쇼핑몰 사이트 새로 리뉴얼 작업에 따라 Cafe24에서 고도몰로 데이터 이전 작업경험.</p>
                               <p class="mb-0">그 경험을 바탕으로 서버가 어떻게 작동하는지 도메인은 무엇인기 배울 수 있었고
                               웹서버를 만드는 방법에 대해서 알게되었습니다.</p>
@@ -50,23 +54,23 @@
                         </div>
                         <div class="flex-shrink-0">
                             <span class="text-primary">March 2014 - May 2014</span>
-                            <div class="scale mx-auto mt-5"><img src="assets/img/clubespresso.jpg" alt="" /></div>
+                            <div class="scale mx-auto mt-5"><a href="http://clubespresso.co.kr"><img src="assets/img/clubespresso.jpg" alt="" ></a></div>
                         </div>
                     </div>
                     <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="flex-grow-1">
                             <h3 class="mb-0">JS 그리고 웹 디자인</h3>
                             <div class="subheading mb-3">JS를 이용한 웹개발</div>
-                            <p>(misosuhi만들었던 사이트 참조)
-                                대학에서 C, C#, Python, Stastics-Analysis, Angular등 기본적인 것들을 공부하고 군대에서 자바스크립트 공부를 시작하였으며
-                                현재 js스크립트를 이용한 프레임워크들을 익히고 있습니다.</p>
-                                <p class="mb-0">또한 웹 디자인 실력이 부족해서 css를 이용한 웹페이지 보다는 부트스트랩 탬플릿을 이용하여 웹 사이트를 
+                            <p>호주에서 유학하던 중 일하던 가게에서 가게를 소개하는 웹사이트를 간단하게 만들어 달라는 부탁을 받아 만들었습니다.
+                                현재는 사이트 운용을 하고있지 않아 저의 도메인에 링크를 걸어 놓았습니다.
+                            </p>
+                                <p class="mb-0"> 웹 디자인 실력이 부족해서 css를 이용한 웹페이지 보다는 부트스트랩 탬플릿을 이용하여 웹 사이트를 
                                 개발하고 있고 추후에 자바스크립트와 프레임워크를 이용하여 웹 사이트를 만들 계획에 있습니다.</p>
                             </p>
                         </div>
                         <div class="flex-shrink-0">
                             <span class="text-primary">March 2020 - Present</span>
-                            <div class="scale mx-auto mt-5"><img src="assets/img/miso.jpg" alt="" /></div>
+                            <div class="scale mx-auto mt-5"><a href="../miso/"><img src="assets/img/miso.jpg" alt="" ></a></div>
                         </div>
                     </div>
                 </div>
@@ -106,27 +110,33 @@
                         <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
                         <li class="list-inline-item"><i class="fab fa-php"></i></li>
                         <li class="list-inline-item"><i class="fab fa-laravel"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
                         <li class="list-inline-item"><i class="fab fa-github"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
                     </ul>
                     <div class="subheading mb-3">우선순위</div>
                     <ul class="fa-ul mb-0">
                         <li>
                             <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Mobile-First, Responsive Design
+                            JS Skill: Intermediate
                         </li>
                         <li>
                             <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Cross Browser Testing & Debugging
+                            JS Skill: Intermediate
                         </li>
                         <li>
                             <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Cross Functional Teams
+                            PHP Skill: Intermediate
                         </li>
                         <li>
                             <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Agile Development & Scrum
+                            MYSQL Skill: Intermediate
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Laravel Skill: Biginner
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Git,github Skill: Pre Intermediate
                         </li>
                     </ul>
                 </div>
@@ -136,8 +146,8 @@
             <section class="resume-section" id="interests">
                 <div class="resume-section-content">
                     <h2 class="mb-5">Interests</h2>
-                    <p></p>
-                    <p class="mb-0"></p>
+                    <p>저의 주된 취미는 실내에서 있는걸 좋아 하기 때문에 책 읽기, 게임 하기, IT관련 Youtube보기 입니다. </p>
+                    <p class="mb-0">밖에서 즐기는 취미로는 테니스, 농구 그리고 방탈출을 즐기고 있습니다.</p>
                 </div>
             </section>
         </div>
